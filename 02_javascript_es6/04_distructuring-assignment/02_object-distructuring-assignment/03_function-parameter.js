c7f9cac7969ab09a77bf9f07293f0af05db93f0c
JaeWon Lee <jlee38266@gmail.com>
2024-03-19 18:38:13 +0900

    js 구조 분해 할당 예제

---
+++ 02_javascript_es6/04_distructuring-assignment/02_object-distructuring-assignment/03_function-parameter.js
@@ -0,0 +1,20 @@
+/* 03. function-parameters(함수의 매개변수에서 구조 분해 할당) */
+
+let exampleProduct = {
+    items: ["Coffee", "Donut"],
+    producer: "신사임당"
+};
+
+function displayProduct({ producer = "아무개", items = [], width = 10, height = 20 }) {
+    
+    console.log(producer);
+    console.log(items);
+    console.log(width);
+    console.log(height);
+    
+}
+
+displayProduct(exampleProduct);
+
+/* 함수 호출 시 객체를 던지면 내부적으로 매개변수로 적힌 구조분해 할당이 일어난다. */
+// let { producer = "아무개", items = [], width = 10, height = 20} = exampleProduct; // 위의 식들이 이렇게 한줄로 작성될 수 있다.
