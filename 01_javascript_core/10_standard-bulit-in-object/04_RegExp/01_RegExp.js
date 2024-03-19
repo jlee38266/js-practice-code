37649e35ea3e1e2ca8a0cb1382fb193f0afa8831
JaeWon Lee <jlee38266@gmail.com>
2024-03-19 11:11:23 +0900

    js built-in-object 정규 표현식 활용

---
+++ 01_javascript_core/10_standard-bulit-in-object/04_RegExp/01_RegExp.js
@@ -0,0 +1,8 @@
+/* 01. RegExp(정규 표현식 리터럴) */
+let regexp = /j/i;  // 패턴: j, 플래그 옵션: i => 대소문자 구별 없이
+
+regexp = new RegExp('j', 'i');
+regexp = new RegExp('/j/', 'i');
+regexp = new RegExp(/j/i);
+
+/* test 메소드 정규 표현식 regexp의 패턴을 검색하여  */
